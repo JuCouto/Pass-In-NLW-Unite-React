51d56b5d1c9be08be5e6be946d536ad20053456f
Ju Couto <jnalmeida@ymail.com>
2024-04-04 23:20:53 -0300

    iniciando header da tabela

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export function AttendeeList () {
   return (
-    <div>AttendeeList</div>
+    <div className="flex gap-3 items-center">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <input className="bg-transparent focus:ring-0 flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar Participante..."/></div>
   )
 }
 
-export default AttendeeList
