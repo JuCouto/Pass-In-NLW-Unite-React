010f7d3a78b5a0675a6e78858257c65488866ef7
Ju Couto <jnalmeida@ymail.com>
2024-04-05 23:10:39 -0300

    modelo array mokado para a tabela

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -23,45 +23,68 @@ const AttendeeList = () => {
       </div>
 
       <div className="border border-white/10 rounded-lg">
-      <table className="w-full ">
-        <thead>
-          <tr className="border-b border-white/10">
-            <th className="py-3 px-4 text-small font-semibold text-left"  >
-              <input
-                type="checkbox"
-                className="size-4 bg-black/20 rounded border border-white/10"
-              />
-            </th>
-            <th className="py-3 px-4 text-small font-semibold text-left">Código</th>
-            <th className="py-3 px-4 text-small font-semibold text-left">Participante</th>
-            <th className="py-3 px-4 text-small font-semibold text-left">Data de inscrição</th>
-            <th className="py-3 px-4 text-small font-semibold text-left">Data do check-in</th>
-            <th className="py-3 px-4 text-small font-semibold text-left" ></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="border-b border-white/10">
-            <td className="py-3 px-4 text-small text-zinc-300">
-              <input type="checkbox" />
+        <table className="w-full ">
+          <thead>
+            <tr className="border-b border-white/10">
+              <th className="py-3 px-4 text-small font-semibold text-left">
+                <input
+                  type="checkbox"
+                  className="size-4 bg-black/20 rounded border border-white/10"
+                />
+              </th>
+              <th className="py-3 px-4 text-small font-semibold text-left">
+                Código
+              </th>
+              <th className="py-3 px-4 text-small font-semibold text-left">
+                Participante
+              </th>
+              <th className="py-3 px-4 text-small font-semibold text-left">
+                Data de inscrição
+              </th>
+              <th className="py-3 px-4 text-small font-semibold text-left">
+                Data do check-in
+              </th>
+              <th className="py-3 px-4 text-small font-semibold text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <tr key={i} className="border-b border-white/10">
+                  <td className="py-3 px-4 text-small text-zinc-300">
+                    <input type="checkbox" />
+                  </td>
+                  <td className="py-3 px-4 text-small text-zinc-300">12345</td>
+                  <td className="py-3 px-4 text-small text-zinc-300">
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">Juliana</span>
+                      <span>juliana@email</span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-4 text-small text-zinc-300">
+                    7 dias atáras
+                  </td>
+                  <td className="py-3 px-4 text-small text-zinc-300">
+                    3 dias atáras
+                  </td>
+                  <td className="py-3 px-4 text-small text-zinc-300"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <td className="py-3 px-4 text-small text-zinc-300" colSpan={3}>
+              Mostrando 10 de 200 itens
             </td>
-            <td className="py-3 px-4 text-small text-zinc-300">12345</td>
-            <td className="py-3 px-4 text-small text-zinc-300">
-                <div className="flex flex-col gap-1">
-              <span className="font-semibold text-white">Juliana</span>
-              <span>juliana@email</span>
-              </div>
+            <td
+              className="py-3 px-4 text-small text-zinc-300 text-right"
+              colSpan={3}
+            >
+              Página 1 de 23
             </td>
-            <td className="py-3 px-4 text-small text-zinc-300">7 dias atáras</td>
-            <td className="py-3 px-4 text-small text-zinc-300">3 dias atáras</td>
-            <td className="py-3 px-4 text-small text-zinc-300"></td>
-          </tr>
-        </tbody>
-        <tfoot >
-            <td className="py-3 px-4 text-small text-zinc-300" colSpan={3}>Mostrando 10 de 200 itens</td>
-            <td className="py-3 px-4 text-small text-zinc-300 text-right" colSpan={3}>Página 1 de 23</td>
-        </tfoot>
-      </table>
-    </div>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 };
