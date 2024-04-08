55182b95c94d44b83defd7a79f70efaa34c839ac
Ju Couto <jnalmeida@ymail.com>
2024-04-08 20:08:24 -0300

    ajuste css botões paginação

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -12,16 +12,20 @@ const IconButton = ({ transparent, ...props }: IconButtonProps) => {
     <button
       //o ...props pega todos os parâmetros menos o transparent.
       {...props}
-      className={
-        transparent
-          ? "bg-black/20 border border-white/10 rounded-md p-1.5"
-          : "bg-white/10 border border-white/10 rounded-md p-1.5"
-      }
-    //   className={twMerge(
-    // "border border-white/10 rounded-md p-1.5",
-    // transparent ? "bg-black/20" : "bg-white/10",
-    // props.disabled ? "opacity-50" : null
-    //   )}
+      // className={
+      //   transparent
+      //     ? "bg-black/20 border border-white/10 rounded-md p-1.5"
+      //     : "bg-white/10 border border-white/10 rounded-md p-1.5"
+      // }
+
+      //twmerge é do tailwind
+      className={twMerge(
+        //classe em comum
+        "border border-white/10 rounded-md p-1.5",
+        // classe que precisa ser alterada
+        transparent ? "bg-black/20" : "bg-white/10",
+        props.disabled ? "opacity-50" : null
+      )}
     />
   );
 };
