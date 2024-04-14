5d342f1041d802a7e0c1814055b77f4e8fab69c4
Ju Couto <jnalmeida@ymail.com>
2024-04-13 22:54:31 -0300

    add fontes

--- nlw-unite-react-native/src/app/_layout.tsx
+++ nlw-unite-react-native/src/app/_layout.tsx
@@ -3,7 +3,31 @@
 
 import "../styles/global.css";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+// Fontes
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function Layout() {
-  return <Slot />; // o Slot pega todas as minhas rotas e repassa
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Slot />
+    </>
+  ); // o Slot pega todas as minhas rotas e repassa
 }
--- nlw-unite-react-native/src/app/index.tsx
+++ nlw-unite-react-native/src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View className="">
-      <Text>index</Text>
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <Text className="text-white text-2xl ">index</Text>
     </View>
   );
 }
