4a3874557fa6b35368da15d1251ae4d7e508206c
Ju Couto <jnalmeida@ymail.com>
2024-04-15 22:22:07 -0300

    link para tela de inscrição

--- nlw-unite-react-native/src/app/index.tsx
+++ nlw-unite-react-native/src/app/index.tsx
@@ -1,11 +1,15 @@
 import { Input } from "@/components/input";
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
+import { Link } from "expo-router"
+import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar style="light" />
       <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
       <View className="w-full mt-12 gap-3">
         <Input>
@@ -16,6 +20,9 @@ export default function Home() {
           />
           <Input.Field placeholder="Código do ingresso" />
         </Input>
+
+        <Button title="Acessar a Credencial" />
+        <Link href="/register" className="text-gray-200 text-base font-bold text-center mt-8">Ainda não possui ingresso?</Link>
       </View>
     </View>
   );
