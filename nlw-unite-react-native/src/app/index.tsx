b970a8de5f3e1e2cb8fdef371d872028c9c7863f
Ju Couto <jnalmeida@ymail.com>
2024-04-15 23:05:08 -0300

    inicio função botões

--- nlw-unite-react-native/src/app/index.tsx
+++ nlw-unite-react-native/src/app/index.tsx
@@ -1,12 +1,22 @@
 import { Input } from "@/components/input";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, Alert } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
 import { Link } from "expo-router"
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 export default function Home() {
+  const [code, setCode] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert("Ingresso", "Informe o código do ingresso")
+    }
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar style="light" />
@@ -18,10 +28,12 @@ export default function Home() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={setCode} />
         </Input>
 
-        <Button title="Acessar a Credencial" />
+        <Button title="Acessar a Credencial" onPress={handleAccessCredential} />
         <Link href="/register" className="text-gray-200 text-base font-bold text-center mt-8">Ainda não possui ingresso?</Link>
       </View>
     </View>
--- nlw-unite-react-native/src/app/register.tsx
+++ nlw-unite-react-native/src/app/register.tsx
@@ -1,16 +1,31 @@
 import { Input } from "@/components/input";
-import { View, Image, Text } from "react-native";
-import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
+import { View, Image, Text, Alert } from "react-native";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
-import { Link } from "expo-router"
+import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert("Inscrição", "Preencha corretamente todos os campos!");
+    }
+    router.push("/ticket")
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar style="light" />
-      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
       <View className="w-full mt-12 gap-3">
         <Input>
           <FontAwesome6
@@ -18,7 +33,7 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Nome completo" />
+          <Input.Field placeholder="Nome completo" onChangeText={setName} />
         </Input>
         <Input>
           <MaterialIcons
@@ -26,10 +41,19 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+          <Input.Field
+            placeholder="E-mail"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+          />
         </Input>
-        <Button title="Realizar a inscrição" />
-        <Link href="/" className="text-gray-200 text-base font-bold text-center mt-8">Já possui um ingresso?</Link>
+        <Button title="Realizar a inscrição" onPress={handleRegister} />
+        <Link
+          href="/"
+          className="text-gray-200 text-base font-bold text-center mt-8"
+        >
+          Já possui um ingresso?
+        </Link>
       </View>
     </View>
   );
