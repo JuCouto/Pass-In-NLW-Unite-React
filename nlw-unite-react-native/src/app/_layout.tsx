4160a436a9c64178878486b5cccc9e54ca934807
Ju Couto <jnalmeida@ymail.com>
2024-04-15 22:21:35 -0300

    tela de cadastro

--- nlw-unite-react-native/src/app/_layout.tsx
+++ nlw-unite-react-native/src/app/_layout.tsx
@@ -3,7 +3,7 @@
 
 import "@/styles/global.css";
 import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+
 import { Loading } from "@/components/loading";
 
 // Fontes
@@ -23,7 +23,6 @@ export default function Layout() {
 
   return (
     <>
-      <StatusBar style="light" />
       {fontsLoaded ? <Slot /> : <Loading />}
     </>
   ); // o Slot pega todas as minhas rotas e repassa
